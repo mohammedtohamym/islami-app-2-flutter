d6a5b60fb92c4888dc3d674754e4f19f790c333f
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-12-15 01:29:30 +0200

    Loaded and displayed hadith files

--- lib/tabs/hadith/hadith_tab.dart
+++ lib/tabs/hadith/hadith_tab.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/app_colors.dart';
+import 'package:islami_app/tabs/hadith/hadith_data_model.dart';
 
-class HadithTab extends StatelessWidget {
+class HadithTab extends StatefulWidget {
+  
+  @override
+  State<HadithTab> createState() => _HadithTabState();
+}
+
+class _HadithTabState extends State<HadithTab> {
+  List<HadithModel> hadithList = [];
+
+
 
   @override
   Widget build(BuildContext context) {
+    if(hadithList.isEmpty){
+      loadHadithFile();
+    }
     return Container(
-      color: Colors.yellow,
+      child: ListView.builder(itemBuilder: (context, index){
+        return Text(hadithList[index].title,
+        style: TextStyle(color: AppColors.primaryDark),);
+      },
+      itemCount: hadithList.length,
+      // scrollDirection: Axis.horizontal,
+      ),
     );
+  }
+
+  void loadHadithFile()async{
+    for(int i = 1 ; i <= 50; i++){
+      String hadethContent = await rootBundle.loadString('assets/files/h$i.txt');
+      List<String> hadithLines = hadethContent.split('\n');
+      String title = hadithLines[0];
+      hadithLines.removeAt(0);
+      HadithModel hadithModel = HadithModel(title: title, content: hadithLines);
+      hadithList.add(hadithModel);
+      setState(() {
+
+      });
+    }
+
   }
 }
